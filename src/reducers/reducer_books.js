6ddebb7c957e862805c1931e928df3511371b618
Frison B Alexander <fri.b.alex@gmail.com>
2018-03-05 11:59:05 -0500

    Get first react-redux connection working

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+//Step 1: create the reducer
+
+export default function() {
+    return [
+        {title: "Molecular Biology of The Cell" },
+        {title: "Introduction to woodworking" },
+        {title: "Basic Neurology" },
+        {title: "Getting started with React" }
+    ]
+}
+
+//Step 2: Wireup the reducer (index.js)
